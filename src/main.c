88f92690c66a5fc9b043d5e9dcce3c2a57396393
Nick Rasmussen <nick-ras@mail.dk>
2022-11-21 08:19:02 -0600

    fixing leaks

--- src/main.c
+++ src/main.c
@@ -69,6 +69,18 @@ int	dfs(t_map *grid, int count_row, int count_col, int door)
 	door = dfs(grid, count_row, count_col + 1, door);
 	return door;
 }
+void free_map(char **map)
+{
+	int i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
 
 void check_map(t_map *grid)
 {
@@ -107,6 +119,7 @@ void check_map(t_map *grid)
 		ft_printf("player cant go to exit, doors%d\n", grid->door_check_recursive);
 		exit (1);
 	}
+	free_map(grid->map2);
 }
 
 t_map *make_grid(t_map *grid, int fd, char *argv)
@@ -114,18 +127,25 @@ t_map *make_grid(t_map *grid, int fd, char *argv)
 	int row_count = 0;
 
 	fd = open(argv, O_RDONLY);
-	grid->map = ft_calloc(grid->rows + 1, sizeof(char)); //maybe not + 1
-	grid->map2 = ft_calloc(grid->rows + 1, sizeof(char)); //maybe not + 1
+	grid->map = ft_calloc(grid->rows, sizeof(char));
 	while (row_count < grid->rows)
 	{
-		grid->map[row_count] = ft_calloc(grid->columns + 1, sizeof(char));
+		grid->map[row_count] = ft_calloc(grid->columns, sizeof(char));
 		grid->map[row_count] = get_next_line(fd);
-		grid->map2[row_count] = ft_calloc(grid->columns + 1, sizeof(char));
-		grid->map2[row_count] = grid->map[row_count];
-		// ft_printf("argc malloced space for %d= %s", row_count, grid->map[row_count]);
 		row_count++;
 	}
 	close(fd);
+	row_count = 0;
+	fd = open(argv, O_RDONLY);
+	grid->map2 = ft_calloc(grid->rows, sizeof(char));
+	while (row_count < grid->rows)
+	{
+		grid->map2[row_count] = ft_calloc(grid->columns, sizeof(char));
+		grid->map2[row_count] = get_next_line(fd);
+		row_count++;
+	}
+	close(fd);
+	ft_printf("\nrows %d col %d\n", grid->rows, grid->columns);
 	return grid;
 }
 
@@ -135,8 +155,8 @@ t_map *get_map_using_gnl(char *argv)
 	int fd = open(argv, O_RDONLY);
 	char *line_as_str = get_next_line(fd);
 	
-	grid = ft_calloc(1, sizeof(t_map));
-	grid->columns = ft_strlen(line_as_str);
+	grid = ft_calloc(1, sizeof *grid); //CHANGED SMTH
+	grid->columns = ft_strlen(line_as_str) - 1;
 	while(line_as_str)
 	{
 	 	ft_printf("%s", line_as_str);
@@ -144,7 +164,13 @@ t_map *get_map_using_gnl(char *argv)
 		line_as_str = get_next_line(fd);
 		grid->rows++;
 	}
+	free(line_as_str);
 	close(fd);
+	if (grid->rows < 4 || grid->columns < 4 || grid->rows == grid->columns)
+	{
+		ft_printf("not enough rows or columns or not a rectangle");
+		exit (1);
+	}
 	grid = make_grid(grid, fd, argv);
 	check_map(grid);
 	return (grid);
@@ -152,12 +178,16 @@ t_map *get_map_using_gnl(char *argv)
 
 int main(int argc, char *argv[])
 {
+	t_map *grid;
+
 	if (argc != 2)
 	{
 		ft_printf("argc == 2 or under\n");
 		return 1;
 	}
 	//test of name of map is..
-	get_map_using_gnl(argv[1]);
+	grid = get_map_using_gnl(argv[1]);
+	free_map(grid->map);
+	free(grid);
 	return 0;
 }
