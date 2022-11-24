1ee42b07d32a659396f8f4677baba22d1128d3e3
nick-ras <nick-ras@mail.dk>
2022-11-24 22:23:58 +0100

    test script works as long as you have mlx.h and libmlx.a in correct root folders

--- src/test.c
+++ src/test.c
@@ -1,38 +1,8 @@
-#include <mlx.h>
-# include "/usr/local/include/mlx.h"
-# include <X11/Xfuncproto.h>
-# include <X11/extensions/shm.h>
-# include <X11/Xlib.h>
-# include <X11/keysym.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <memory.h>
+#include "mlx.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-int	main(void)
+int main()
 {
-	void	*mlx;
-	t_data	img;
-
-	mlx = mlx_init();
-	img.img = mlx_new_image(mlx, 1920, 1080);
-
-	/*
-	** After creating an image, we can call `mlx_get_data_addr`, we pass
-	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	** then be set accordingly for the *current* data address.
-	*/
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+    void *mlx = mlx_init();
+    void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window");
+    mlx_loop(mlx);
 }
